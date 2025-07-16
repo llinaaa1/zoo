ad9dd30ceb5b9ebb97f64474ef8d5d28135b6dab
llinaaa1 <lashenova.polina@gmail.com>
2025-07-16 13:50:37 +0300

    Add files via upload

---
+++ sw (2).js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'zoo-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/tickets.html',
+  '/tickets.css',
+  '/style.css',
+  '/animals.png',
+  '/nature.png',
+  '/education.png',
+  '/leo.png',
+  '/panda.png',
+  '/giraffe.png',
+  '/zoo.jpg',
+  '/zoo512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
